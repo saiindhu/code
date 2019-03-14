9f364db4816bfcd9cd7e38494460fc907cf3fcaf
saiindhu <46838993+saiindhu@users.noreply.github.com>
2019-03-14 15:40:03 +0530

    multipleof_13.c

---
+++ multipleof_13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("Yes");
+        
+    }
+    else
+    {
+        printf("No");
+    }
+    getch();
+}
